e536de2b720507144f4231b5e0bd81015e209c90
krr3804 <seokju73@gmail.com>
2023-02-21 11:07:25 +0900

    Fix: Reservation 서버로 보내는 DTO 수정

--- src/main/java/com/ticketaka/performance/dto/ReservationDTO.java
+++ src/main/java/com/ticketaka/performance/dto/ReservationDTO.java
@@ -16,6 +16,7 @@ import java.time.LocalDate;
 public class ReservationDTO {
     private String memberId;
     private String memberEmail;
+    private String performanceId;
     private int reservationTicketCount;
     private LocalDate reservationDate;
     private String reservationTime;
@@ -26,6 +27,7 @@ public class ReservationDTO {
         return ReservationDTO.builder()
                 .memberId(request.getMemberId())
                 .memberEmail(request.getMemberEmail())
+                .performanceId(request.getPerformanceId())
                 .reservationTicketCount(count)
                 .reservationDate(prfSession.getPrfSessionDate())
                 .reservationTime(prfSession.getPrfSessionTime())
