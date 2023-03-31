3f53c324e99d6b965eb898fb6a69123fc100c006
krr3804 <seokju73@gmail.com>
2023-03-31 17:32:03 +0900

    Fix: 예약 생성 에러 수정

--- src/main/java/com/ticketaka/performance/feign/ReservationFeignClient.java
+++ src/main/java/com/ticketaka/performance/feign/ReservationFeignClient.java
@@ -3,6 +3,7 @@ package com.ticketaka.performance.feign;
 
 import com.ticketaka.performance.dto.ReservationDTO;
 import com.ticketaka.performance.dto.response.BaseResponse;
+import feign.HeaderMap;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,5 +14,5 @@ import java.util.Map;
 @FeignClient(name="ReservationFeignClient", url="${member.url}:"+"${member.port}", path = "/reservation")
 public interface ReservationFeignClient {
     @PostMapping("/create")
-    BaseResponse createReservation(@RequestHeader Map<String,String> header, @RequestBody ReservationDTO reservationDTO);
+    BaseResponse createReservation(@HeaderMap Map<String,String> header, @RequestBody ReservationDTO reservationDTO);
 }
